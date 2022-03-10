a3321560a7c651519b786d59018db30b7c0617fd
Greicy Itakura <greicyitakura@gmail.com>
2022-03-10 18:59:36 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/sdsmovie/SdsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/sdsmovie/SdsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
